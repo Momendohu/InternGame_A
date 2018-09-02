61f0fa26c72cba59b7d3dfe912b7927d63449b86
阿保達也 <pinog.jh@gmail.com>
2018-09-02 22:25:55 +0900

    パネル生成

---
+++ Assets/ABO/Scripts/Panel.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+
+public class Panel : MonoBehaviour {
+    //=============================================================
+    private Color onColor = new Color(97f / 255f,195f / 255f,89f / 255f);
+    private Color offColor = new Color(255f / 255f,236f / 255f,185f / 255f);
+
+    //=============================================================
+    [System.NonSerialized]
+    public PanelManager.EState State = PanelManager.EState.off; //状態
+
+    //=============================================================
+    private SpriteRenderer spriteRenderer;
+
+    //=============================================================
+    private void Init () {
+        CRef();
+    }
+
+    //=============================================================
+    private void CRef () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    //=============================================================
+    private void Awake () {
+        Init();
+    }
+
+    private void Start () {
+
+    }
+
+    private void Update () {
+        switch(State) {
+            case PanelManager.EState.off:
+            spriteRenderer.color = offColor;
+            break;
+
+            case PanelManager.EState.on:
+            spriteRenderer.color = onColor;
+            break;
+
+            default:
+            break;
+        }
+    }
+}
--- Assets/ABO/Scripts/PanelManager.cs
+++ Assets/ABO/Scripts/PanelManager.cs
@@ -4,29 +4,60 @@ using UnityEngine;
 //using UnityEngine.UI;
 
 public class PanelManager : MonoBehaviour {
-	//=============================================================
-	
+    //=============================================================
+    private int panelNumX = 11; //パネルの数
+    private int panelNumY = 11; //パネルの数
+    private float panelX = 0; //パネルの位置
+    private float panelY = 0; //パネルの位置
+    private float panelSize = 0.48f; //パネルのサイズ
+    private float panelGap = 0.015f; //パネル間のすきま
 
-	//=============================================================
-	private void Init(){
-		CRef();
-	}
+    //=============================================================
+    public enum EState { off = 0, on = 1 } //状態
 
-	//=============================================================
-	private void CRef(){
-		
-	}
+    //=============================================================
+    private GameObject panel; //パネル
 
-	//=============================================================
-	private void Awake () {
-		Init();
-	}
+    //=============================================================
+    private void Init () {
+        CRef();
+    }
 
-	private void Start () {
-		
-	}
-	
-	private void Update () {
-		
-	}
+    //=============================================================
+    private void CRef () {
+        panel = Resources.Load("Panel") as GameObject;
+    }
+
+    //=============================================================
+    private void Awake () {
+        Init();
+    }
+
+    private void Start () {
+        CreatePanels(panelNumX,panelNumY);
+    }
+
+    private void Update () {
+
+    }
+
+    //=============================================================
+    //パネルの生成
+    //numX:横の数
+    //numY:縦の数
+    private void CreatePanels (int numX,int numY) {
+        GameObject obj = null;
+
+        for(int i = 0;i < numX;i++) {
+            for(int j = 0;j < numY;j++) {
+                obj = Instantiate(panel) as GameObject;
+                obj.transform.position = new Vector2((panelSize + panelGap) * (i - numX / 2) + panelX,(panelSize + panelGap) * (j - numY / 2) + panelY);
+                if(i == 2) {
+                    obj.GetComponent<Panel>().State = EState.on;
+                }
+
+                Debug.Log(i+":"+j);
+            }
+        }
+    }
 }
