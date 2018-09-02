22e0cf18dc1cc87536ae1c84eb82048847a7a11c
阿保達也 <pinog.jh@gmail.com>
2018-09-03 01:11:22 +0900

    微変更

--- Assets/Nagata/Script/Mouse.cs
+++ Assets/Nagata/Script/Mouse.cs
@@ -4,26 +4,25 @@ using UnityEngine;
 
 public class Mouse : MonoBehaviour {
 
-    [SerializeField]private float Cursol_X;
-    [SerializeField]private float Cursol_Y;
-    [SerializeField]private bool TapFlag;
+    [SerializeField] private float Cursol_X;
+    [SerializeField] private float Cursol_Y;
+    [SerializeField] private bool TapFlag;
 
     // Use this for initialization
     void Start () {
         MouseInfo();
         TapFlag = false;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
         MouseInfo();
     }
 
     //---------------------------------------------------------
     // マウスの座標やクリックの管理
     //---------------------------------------------------------
-    private void MouseInfo()
-    {
+    private void MouseInfo () {
         Vector3 vPos = Input.mousePosition;
         vPos.z = 10.0f;
 
@@ -32,25 +31,23 @@ public class Mouse : MonoBehaviour {
         Cursol_X = vWorldPos.x;
         Cursol_Y = vWorldPos.y;
         TapFlag = Input.GetMouseButton(0);  // 左ボタンクリック
-        this.transform.position = new Vector3(Cursol_X, Cursol_Y, vWorldPos.z); 
+        this.transform.position = new Vector3(Cursol_X,Cursol_Y,vWorldPos.z);
     }
 
     //--------------------------------------------------------
     // 侵入検知(領域に入っているとき)
     //--------------------------------------------------------
-    void OnTriggerStay2D(Collider2D other)
-    {
+    void OnTriggerStay2D (Collider2D other) {
         // 左クリックonの時
         //if(TapFlag)
         {
-             //配置ブロックの色を変える
-            if (other.tag  == "Panel")
-            {
+            //配置ブロックの色を変える
+            if(other.tag == "Panel") {
                 other.gameObject.GetComponent<Panel>().ChangeState(PanelManager.EState.on);
                 Debug.Log("当たった");
                 //other.gameObject.GetComponent<SpriteRenderer>().color = Color.red;
             }
-        } 
+        }
     }
 
     //void RayTest()
